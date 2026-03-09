6803bce19858fdcf7eb235c899bc267fb5d65a76
naugtur <naugtur@gmail.com>
2026-03-09 10:46:13 +0100

    new chromium was crashing the tab with too much depth

--- memory/leaks-browser/ex2.js
+++ memory/leaks-browser/ex2.js
@@ -1,36 +1,36 @@
 window.module = (() => {
+  let last = document.querySelector("#wrapper");
 
-    let last = document.querySelector('#wrapper');
+  return {
+    run() {
+      let d;
+      for (let k = 0; k < 10; k++) {
+        for (let i = 0; i < 10000; i++) {
+          d = document.createElement("span");
+          d.textContent = i;
 
-
-    return {
-        run() {
-            let i, d;
-            for (i = 0; i < 1000; i++) {
-                d = document.createElement('div');
-                d.textContent = i;
-
-                last.appendChild(d);
-                last = d;
-            }
-
-            d = document.createElement('span');
-            d.textContent = 'last in tree';
-
-            last.appendChild(d);
-            last = d;
-
-        },
-        clean() {
-            const wrapper = document.querySelector('#wrapper');
-            wrapper.parentNode.removeChild(wrapper); //empty
-        },
-        realClean() {
-            const wrapper = document.querySelector('#wrapper');
-            if (wrapper) {
-                wrapper.parentNode.removeChild(wrapper); //empty
-            }
-            last = null;
+          last.appendChild(d);
         }
-    }
+        last = d;
+
+        d = document.createElement("div");
+        d.textContent = " level in tree ";
+        d.style = "border: 1px solid red; padding: 5px; margin: 5px;";
+
+        last.appendChild(d);
+        last = d;
+      }
+    },
+    clean() {
+      const wrapper = document.querySelector("#wrapper");
+      wrapper.parentNode.removeChild(wrapper); //empty
+    },
+    realClean() {
+      const wrapper = document.querySelector("#wrapper");
+      if (wrapper) {
+        wrapper.parentNode.removeChild(wrapper); //empty
+      }
+      last = null;
+    },
+  };
 })();
--- memory/leaks-browser/ex3.js
+++ memory/leaks-browser/ex3.js
@@ -1,35 +1,35 @@
 window.module = (function () {
+  let last = document.querySelector("#wrapper");
 
-    let last = document.querySelector('#wrapper');
+  return {
+    run() {
+      let d;
+      for (let k = 0; k < 10; k++) {
+        for (let i = 0; i < 10000; i++) {
+          d = document.createElement("span");
+          d.textContent = i;
 
-
-    return {
-        run() {
-            let i, d;
-            for (i = 0; i < 1000; i++) {
-                d = document.createElement('div');
-                d.textContent = i;
-
-                last.appendChild(d);
-                last = d;
-            }
-
-            d = document.createElement('span');
-            d.textContent = 'last in tree';
-            // name your functions to make leak searching easier
-            window.addEventListener('unload', function someFunction() {
-                console.log(i, d);
-                //Note: browsers remove references from function context if they are not used inside the function. But you shouldn't bet on that...
-            })
-
-            last.appendChild(d);
-            last = d;
-
-        },
-        clean() {
-            const wrapper = document.querySelector('#wrapper');
-            wrapper.parentNode.removeChild(wrapper); //empty
-            last = null;
+          last.appendChild(d);
         }
-    }
+        last = d;
+
+        d = document.createElement("div");
+        d.textContent = " level in tree ";
+        d.style = "border: 1px solid red; padding: 5px; margin: 5px;";
+
+        last.appendChild(d);
+        last = d;
+      }
+      // name your functions to make leak searching easier
+      window.addEventListener("unload", function someFunction() {
+        console.log(d);
+        //Note: browsers sometimes remove references from function context if they are not used inside the function. But you shouldn't bet on that...
+      });
+    },
+    clean() {
+      const wrapper = document.querySelector("#wrapper");
+      wrapper.parentNode.removeChild(wrapper); //empty
+      last = null;
+    },
+  };
 })();
